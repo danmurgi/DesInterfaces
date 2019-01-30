c3ebd19c644e97ed2846d9e71dd93d04032ba636
Dani Rubio <zanedan@hotmail.com>
2019-01-30 11:53:02 +0100

    cambio

--- GraficoSector/GraficoSector/Grafico.cs
+++ GraficoSector/GraficoSector/Grafico.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -18,6 +19,7 @@ namespace GraficoSector
     public sealed class Grafico : Control
     {
         Canvas paleta;
+        Ellipse circunf;
         public Grafico()
         {
             this.DefaultStyleKey = typeof(Grafico);
@@ -30,30 +32,88 @@ namespace GraficoSector
 
             if(paleta != null)
             {
-                PintarGrafico();
+                trazar();
+                //PintarGrafico();
+
             }
             
         }
 
         private void PintarGrafico()
         {
-            Ellipse elipse = new Ellipse();
+            //Creamos la ellipse
+            circunf = new Ellipse();
 
-            elipse.Width = 300;
-            elipse.Height = 300;
+            //Redimensionamos la ellipse
+            circunf.Width = 300;
+            circunf.Height = 300;
 
+            //Obtenemos el color
             SolidColorBrush color = new SolidColorBrush(Colors.Aqua);
-            //color.Color = Color.FromArgb(0, 0, 0, 0);
 
-            elipse.Fill = color;
+            //Pintamos la ellipse
+            circunf.Fill = color;
 
-            //Canvas.SetLeft(elipse, 200);
-            //Canvas.SetTop(elipse, 200);
-
-            paleta.Children.Add(elipse);
-
-            Canvas.SetLeft(elipse, 50);
-            Canvas.SetTop(elipse, 50);
+            //Añadimos la ellipse al canvas
+            paleta.Children.Add(circunf);
+            
+            //Posicionamos la ellipse
+            Canvas.SetLeft(circunf, 50);
+            Canvas.SetTop(circunf, 50);
         }
+        
+        //De momento se le pasa un numero de productos, mas tarde se le pasara una lista de objetos
+        private void trazar(int numProductos)
+        {
+            //int x
+            for (int i = 0; i < numProductos; i++)
+            {
+                //Instanciamos el poligono
+                Polygon poligono = new Polygon();
+
+                //Creamos los colores
+                SolidColorBrush rojo = new SolidColorBrush(Colors.Red);
+                SolidColorBrush verde = new SolidColorBrush(Colors.Green);
+
+                //Pintamos el borde y relleno del poligono
+                poligono.Stroke = rojo;
+                poligono.Fill = verde;
+
+                //poligono.StrokeThickness = 2;
+
+                //Creamos los puntos
+                Point punto1 = new Point(0, 0);
+                Point punto2 = new Point(0, 100);
+                Point punto3 = new Point(100, 100);
+
+                //Creamos una lista de puntos
+                PointCollection listaPuntos = new PointCollection();
+
+                //Añadimos los puntos
+                listaPuntos.Add(punto1);
+                listaPuntos.Add(punto2);
+                listaPuntos.Add(punto3);
+
+                //Le asignamos la lista de puntos al poligono
+                poligono.Points = listaPuntos;
+
+                //Añadimos el poligono al canvas
+                paleta.Children.Add(poligono);
+            }
+        }
+
+
+        //Propiedad que define el alto y ancho del grafico
+        public int tamano
+        {
+            get { return (int)GetValue(tamanoProperty); }
+            set { SetValue(tamanoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for tamano.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty tamanoProperty =
+            DependencyProperty.Register("tamano", typeof(int), typeof(Grafico), new PropertyMetadata(null));
+
+
     }
 }

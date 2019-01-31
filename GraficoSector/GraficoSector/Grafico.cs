556f7bd15b9f2398846772d1659dd360cdcf0837
Dani Rubio <zanedan@hotmail.com>
2019-01-31 09:23:32 +0100

    1

--- GraficoSector/GraficoSector/Grafico.cs
+++ GraficoSector/GraficoSector/Grafico.cs
@@ -12,12 +12,11 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
-// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
 
 namespace GraficoSector
 {
     public sealed class Grafico : Control
-    {
+    { 
         Canvas paleta;
         Ellipse circunf;
         public Grafico()
@@ -32,86 +31,17 @@ namespace GraficoSector
 
             if(paleta != null)
             {
-                trazar(4);
+                //trazar(4);
                 //PintarGrafico();
+                trazarGrafico();
 
             }
             
         }
 
-        private void PintarGrafico()
+        private void trazarGrafico()
         {
-            //Creamos la ellipse
-            circunf = new Ellipse();
-
-            //Redimensionamos la ellipse
-            circunf.Width = 300;
-            circunf.Height = 300;
-
-            //Obtenemos el color
-            SolidColorBrush color = new SolidColorBrush(Colors.Aqua);
-
-            //Pintamos la ellipse
-            circunf.Fill = color;
-
-            //Añadimos la ellipse al canvas
-            paleta.Children.Add(circunf);
             
-            //Posicionamos la ellipse
-            Canvas.SetLeft(circunf, 50);
-            Canvas.SetTop(circunf, 50);
-        }
-        
-        //De momento se le pasa un numero de productos, mas tarde se le pasara una lista de objetos
-        private void trazar(int numProductos)
-        {
-            int tamanoTotal = 300;
-            int cantidadMax = 2000;
-            int auxEje = 0;
-            int yPunto2 = tamanoTotal/2;
-            int yPunto3 = 100;
-
-            for (int i = 0; i < numProductos; i++)
-            {
-                //Instanciamos el poligono
-                Polygon poligono = new Polygon();
-
-                //Creamos los colores
-                SolidColorBrush rojo = new SolidColorBrush(Colors.Red);
-                SolidColorBrush verde = new SolidColorBrush(Colors.Green);
-
-                //Pintamos el borde y relleno del poligono
-                poligono.Stroke = rojo;
-                poligono.Fill = verde;
-
-                //poligono.StrokeThickness = 2;
-
-                //Creamos los puntos
-                //if ()
-                //{
-
-                //}
-                Point punto1 = new Point(0, 0);
-                Point punto2 = new Point(0, yPunto2);
-                Point punto3 = new Point(tamanoTotal/2, yPunto3);
-
-                //Creamos una lista de puntos
-                PointCollection listaPuntos = new PointCollection();
-
-                //Añadimos los puntos
-                listaPuntos.Add(punto1);
-                listaPuntos.Add(punto2);
-                listaPuntos.Add(punto3);
-
-                //Le asignamos la lista de puntos al poligono
-                poligono.Points = listaPuntos;
-
-                //Añadimos el poligono al canvas
-                paleta.Children.Add(poligono);
-
-                yPunto2+=
-                yPunto3 += 100;
-            }
         }
 
 
---
+++ GraficoSector/GraficoSector/Videojuego.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficoSector
+{
+    public class Videojuego
+    {
+        public string nombre { get; set; }
+        public int ventas { get; set; }
+
+    }
+}
